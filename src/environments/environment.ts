82c9ae570badd4ccd9c28c87ec175dc35047eff5
Cris <krontroth@gmail.com>
2026-05-22 14:34:07 -0500

    bkn settings

---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://csvbkn.onrender.com/api' // Usaremos el de producción también en local por ahora, o puedes cambiarlo a localhost
+};
