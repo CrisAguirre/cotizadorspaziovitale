415dffc2550a4a0b375ac8c44c2fc1c820ffecf7
Cris <krontroth@gmail.com>
2026-07-01 00:55:11 -0500

    77

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { QuotationService } from '../../services/quotation.service';
 import { DashboardStats } from '../../models/interfaces';
 
@@ -7,15 +8,49 @@ import { DashboardStats } from '../../models/interfaces';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
   stats: DashboardStats | null = null;
   isLoading = true;
   viewMode: 'month' | 'all' = 'month';
 
-  constructor(private quotationService: QuotationService) {}
+  currentTime: Date = new Date();
+  currentTrm: number | null = null;
+  private timerRef: any;
+
+  constructor(
+    private quotationService: QuotationService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
     this.loadStats();
+    this.startClock();
+    this.fetchTrm();
+  }
+
+  ngOnDestroy(): void {
+    if (this.timerRef) {
+      clearInterval(this.timerRef);
+    }
+  }
+
+  startClock(): void {
+    this.timerRef = setInterval(() => {
+      this.currentTime = new Date();
+    }, 1000);
+  }
+
+  fetchTrm(): void {
+    // API de Datos Abiertos de Colombia
+    const url = 'https://www.datos.gov.co/resource/32sa-8pi3.json?$limit=1&$order=vigenciahasta%20DESC';
+    this.http.get<any[]>(url).subscribe({
+      next: (data) => {
+        if (data && data.length > 0) {
+          this.currentTrm = parseFloat(data[0].valor);
+        }
+      },
+      error: (err) => console.error('Error fetching TRM', err)
+    });
   }
 
   loadStats(): void {
